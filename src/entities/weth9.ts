6fdb03be1898c0044848c7c7a6a17bdf27e832f5
Gustavo Gallegos <meaning.app>
2021-06-02 19:07:17 -0700

    Revert "Merge branch 'main' of github.com:nftgem/unigem-sdk-core into main"
    This reverts commit 1793f21baa7003853d521328244bd20807c9a0e6, reversing
    changes made to 70db4d86fb89e5ca7dda07656ebfbb8659c7ddef.

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -8,6 +8,5 @@ export const WETH9: { [chainId: number]: Token } = {
   [3]: new Token(3, '0xc778417E063141139Fce010982780140Aa0cD5Ab', 18, 'WETH9', 'Wrapped Ether'),
   [4]: new Token(4, '0xc778417E063141139Fce010982780140Aa0cD5Ab', 18, 'WETH9', 'Wrapped Ether'),
   [5]: new Token(5, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH9', 'Wrapped Ether'),
-  [42]: new Token(42, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH9', 'Wrapped Ether'),
-  [250]: new Token(250, '0x7be3e14833E2cE07f41451e16696CE8BF55dD785', 18, 'WETH9', 'Wrapped Ether')
+  [42]: new Token(42, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH9', 'Wrapped Ether')
 }
